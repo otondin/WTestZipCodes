14d8d928e443e570a52a5d19fbbda497fc280042
GABRIEL TONDIN <gabriel.tondin@mercedes-benz.io>
2021-10-29 12:22:54 +0100

    Initial Commit

---
+++ WTest_Gabriel_TondinTests/WTest_Gabriel_TondinTests.swift
@@ -0,0 +1,33 @@
+//
+//  WTest_Gabriel_TondinTests.swift
+//  WTest_Gabriel_TondinTests
+//
+//  Created by Gabriel Tondin on 29/10/2021.
+//
+
+import XCTest
+@testable import WTest_Gabriel_Tondin
+
+class WTest_Gabriel_TondinTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ WTest_Gabriel_TondinUITests/WTest_Gabriel_TondinUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WTest_Gabriel_TondinUITestsLaunchTests.swift
+//  WTest_Gabriel_TondinUITests
+//
+//  Created by Gabriel Tondin on 29/10/2021.
+//
+
+import XCTest
+
+class WTest_Gabriel_TondinUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
